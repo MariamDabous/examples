4ec115cacaa72064cd8689869a2df4666ae09973
MariamDabous <mariam.dabous@hotmail.com>
2022-08-29 15:08:32 +0300

    initiall

---
+++ JS/Loop/loop.js
@@ -0,0 +1,33 @@
+//odd
+console.log("Odd");
+for(var i=1; i<=20;i++){
+    if(i%2!==0){
+        console.log(i);
+    }
+}
+console.log("Multiples of 3");
+for(var i=100; i>=0;i--){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+console.log("Sequence");
+for(var i=4; i>-4; i-=1.5){
+    console.log(i);
+}
+
+console.log("Sigma");
+var sum=0;
+for(var i=1; i<=100; i++){
+    sum+=i;
+}
+console.log(sum);
+
+console.log("Factorial");
+var fact=1;
+for(var i=1; i<=12; i++){
+    fact*=i;
+}
+console.log(fact);
+
