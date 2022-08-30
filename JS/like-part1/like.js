4bfde91458653761b6ddafa4405b75c267b9622d
MariamDabous <mariam.dabous@hotmail.com>
2022-08-30 16:58:28 +0300

    final

---
+++ JS/like-part1/like.js
@@ -0,0 +1,11 @@
+
+var count = document.querySelector("#likes");
+    var i=3;
+    console.log(count);// for programmer (not affected the code)
+
+
+function inc(){
+    i+=1;
+    count.innerText= i+"like(s)";
+    console.log(i);//// for programmer (not affected the code)   
+}
