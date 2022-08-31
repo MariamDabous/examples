e1df904c6588e8a455a97f391f890162dab3495c
MariamDabous <mariam.dabous@hotmail.com>
2022-08-31 13:14:47 +0300

    modify

--- JS/Loop/loop.js
+++ JS/Loop/loop.js
@@ -30,4 +30,3 @@ for(var i=1; i<=12; i++){
     fact*=i;
 }
 console.log(fact);
-
---
+++ JS/profile-page/script.js
@@ -0,0 +1,17 @@
+console.log("page loaded...");
+
+var changename = document.querySelector("#name");
+function change(){
+    changename.innerText = "Ahmad" ;
+    console.log("xy");
+}
+
+var requestname = document.querySelector("#todd")
+function rem1(){
+    requestname.remove();
+}
+
+var requestname1 = document.querySelector("#phil")
+function rem2(){
+    requestname1.remove();
+}
