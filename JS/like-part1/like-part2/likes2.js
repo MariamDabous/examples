5cbe910044aad613badbe6ade11b6bd98a6a8f91
MariamDabous <mariam.dabous@hotmail.com>
2022-08-31 08:45:08 +0300

    final

---
+++ JS/like-part1/like-part2/likes2.js
@@ -0,0 +1,32 @@
+var count = document.querySelector("#likes1");
+    var i=9;
+    console.log(count);// for programmer (not affected the code)
+
+
+function inc1(){
+    i+=1;
+    count.innerText= i+"like(s)";
+    console.log(i);//// for programmer (not affected the code)   
+}
+
+var count2 = document.querySelector("#likes2");
+    var n=12;
+    console.log(count2);// for programmer (not affected the code)
+
+
+function inc2(){
+    n+=1;
+    count2.innerText= n+"like(s)";
+    console.log(n);//// for programmer (not affected the code)   
+}
+
+var count3 = document.querySelector("#likes3");
+    var m=9;
+    console.log(count3);// for programmer (not affected the code)
+
+
+function inc3(){
+    m+=1;
+    count3.innerText= m+"like(s)";
+    console.log(m);//// for programmer (not affected the code)   
+}
